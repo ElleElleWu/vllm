09ce5b0d9d819bd7dc885916a5dcc54887bc9dc8
jiangkuaixue123 <jiangxiaozhou111@163.com>
2025-11-28 09:24:58 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- vllm/model_executor/layers/rotary_embedding/deepseek_scaling_rope.py
+++ vllm/model_executor/layers/rotary_embedding/deepseek_scaling_rope.py
@@ -137,7 +137,8 @@ class DeepseekScalingRotaryEmbedding(RotaryEmbeddingBase):
             sin = sin.repeat_interleave(2, dim=-1).unsqueeze(-2)
 
         logger.info(f"jcz forward_native cos shape:{cos.shape} sin shape:{sin.shape} "
-                    f"positions shape:{positions.shape} query shape:{query.shape} key shape:{key.shape}")
+                    f"positions shape:{positions.shape} query shape:{query.shape} key shape:{key.shape}"
+                    f" query_rot:{query_rot.shape} key_rot:{key_rot.shape}")
         rotate_fn = rotate_neox if self.is_neox_style else rotate_gptj
         query_rot = query_rot * cos + rotate_fn(query_rot) * sin
         key_rot = key_rot * cos + rotate_fn(key_rot) * sin
